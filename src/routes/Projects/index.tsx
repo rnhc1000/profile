838a250bebe012faeae23645c57b5902e90a5a6b
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-10-24 20:54:46 -0300

    feat(Projects) desktop version ready

---
+++ src/routes/Projects/index.tsx
@@ -0,0 +1,14 @@
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import Project from "../../components/Project";
+
+export default function Projects() {
+
+    return (
+        <>
+            <Header />
+            <Project />
+            <Footer />
+        </>
+    );
+}
