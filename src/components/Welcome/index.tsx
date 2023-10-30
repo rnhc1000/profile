ba314a74a957d5413a55c8059a481bfb50a8ca7f
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-10-29 21:54:22 -0300

    feat(Responsiveness) step one... 320px max-width

--- src/components/Welcome/index.tsx
+++ src/components/Welcome/index.tsx
@@ -6,8 +6,9 @@ import Zoom from 'react-reveal/Zoom';
 // @ts-ignore
 import Rotate from 'react-reveal/Rotate';
 import contactLogo from '../../assets/whatsapp.svg';
-import projectsLogo from '../../assets/activity.svg';
-import skillsLogo from '../../assets/tools.svg';
+import codeLogo from '../../assets/activity.svg';
+import skillLogo from '../../assets/tools.svg';
+
 
 export default function Welcome() {
 
@@ -97,15 +98,16 @@ export default function Welcome() {
                     <Zoom>
                         <p className="welcome-important">
                             If you want to go down to the nitty-gritty of my
-                            challenging career as a solutions developer, keep in touch clicking at my <span>contacts </span><Link to="/contacts"> <img src={contactLogo}></img></Link>  or 
-                            take a quick look at the current projects I am working on, clicking here<Link to="/projects"> <img src={projectsLogo}/></Link> or 
-                            even take a look at my current <span>skills</span>
-                            <Link to="/skills">
-                                <span className="resume">
-                                    <img src={skillsLogo}/>
-                                </span>
-                            </Link>
+                            challenging career as a solutions developer, text me, call me or drop me a quick message and 
+                            I will return as soon as possible.
                         </p>
+                    </Zoom>
+                    <Zoom>
+                        <div className="contact-me">
+                            <Link to="/projects"><img src={codeLogo}></img></Link>
+                            <Link to="/skills"><img src={skillLogo}></img></Link>
+                            <Link to="/contacts"><img src={contactLogo}></img></Link>
+                        </div>
                     </Zoom>
                 </div>
             </section>
