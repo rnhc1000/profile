1fd72753d4ee0b1c054c83b141282222498e91fd
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-12-31 17:01:39 -0300

    Added prism.js and prism.css to highlight a code snippet at the welcome page

--- src/components/Welcome/index.tsx
+++ src/components/Welcome/index.tsx
@@ -8,6 +8,9 @@ import Rotate from 'react-reveal/Rotate';
 import contactLogo from '../../assets/whatsapp.svg';
 import codeLogo from '../../assets/activity.svg';
 import skillLogo from '../../assets/tools.svg';
+import history from '../../assets/history.svg';
+import prism from 'prismjs';
+import "../../css/prism.css";
 
 
 export default function Welcome() {
@@ -15,25 +18,36 @@ export default function Welcome() {
     return (
         <section id="welcome-section">
             <div className="welcome-container">
-                <p className="welcome-important"></p>
-                    <Zoom>
-                        <dt> Hi everyone!</dt>
-                        <blockquote>
-                            I am called <span>Ricardo Ferreira</span> and as a <Link to="https://www.ferreiras.dev.br/cv/ResumeRicardoFerreira.pdf" target="_blank" className="contact-details"> Full Stack Software Engineer</Link>,
-                            🛠️ love to design, write clean code and deploy business applications.
-                            I have been very succesful in being the architect of solutions that have met and even exceeded businesses
-                            and customers requirements in all companies I have been working for, since the very beginning of my career
-                            as a Junior Engineer.
-                        </blockquote>
-                    </Zoom>
+                
+                <Zoom>
+                    <pre>
+                        <code className="language-javadoc">{`
+        class HelloWorld {
+            public static main void(String... args) {
+                System.out.println("Hello everybody!");
+            }
+        }
+                            `}
+                        </code>
+                    </pre>
+                    <br></br>
 
-                    <Zoom>
-                        <div className="contact-me">
-                            <Link to="/projects"><img src={codeLogo}></img></Link>
-                            <Link to="/skills"><img src={skillLogo}></img></Link>
-                            <Link to="/contacts"><img src={contactLogo}></img></Link>
-                        </div>
-                    </Zoom>
+                    <blockquote>
+                        I am called <span>Ricardo Ferreira</span> and as a <Link to="https://www.ferreiras.dev.br/cv/ResumeRicardoFerreira.pdf" target="_blank" className="contact-details"> Sr Software Engineer</Link>,
+                        🛠️ loves to design, writes clean code and deploy web-based business applications.
+                        I have been very succesful in being the architect of solutions and applications that have met and even exceeded businesses
+                        and customers requirements in all companies I have been working for. For a comprehensive description of my journey,
+                        take a look at my history, clicking here. <Link to="/skills"><img src={history}></img></Link>
+                    </blockquote>
+                </Zoom>
+
+                <Zoom>
+                    <div className="contact-me">
+                        <Link to="/projects"><img src={codeLogo}></img></Link>
+                        <Link to="/skills"><img src={skillLogo}></img></Link>
+                        <Link to="/contacts"><img src={contactLogo}></img></Link>
+                    </div>
+                </Zoom>
             </div>
         </section>
     );
