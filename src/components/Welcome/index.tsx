a66103a81ba3aba021963d8663469319c61356ef
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-12-31 20:44:10 -0300

    Added History and refactored home page

--- src/components/Welcome/index.tsx
+++ src/components/Welcome/index.tsx
@@ -4,45 +4,71 @@ import { Link } from 'react-router-dom';
 // @ts-ignore
 import Zoom from 'react-reveal/Zoom';
 // @ts-ignore
-import Rotate from 'react-reveal/Rotate';
 import contactLogo from '../../assets/whatsapp.svg';
 import codeLogo from '../../assets/activity.svg';
 import skillLogo from '../../assets/tools.svg';
 import history from '../../assets/history.svg';
-import prism from 'prismjs';
-import "../../css/prism.css";
+// import Prism from 'prismjs';
+// import 'prismjs/components/prism-clike';
+// import 'prismjs/themes/prism.css';
+
+//import 'prismjs/plugins/line-numbers/prism-line-numbers.js'
+
+// import "../../css/prism.css";
+import { useEffect } from 'react';
 
 
 export default function Welcome() {
+    // useEffect(() => {
+    //     Prism.highlightAll()
+    //   }, [])
 
     return (
         <section id="welcome-section">
             <div className="welcome-container">
-                
+
                 <Zoom>
                     <pre>
-                        <code className="language-javadoc">{`
-        class HelloWorld {
-            public static main void(String... args) {
-                System.out.println("Hello everybody!");
+                        <code className="welcome">{`
+        interface Hello{ 
+            String hello();
+        }
+
+        public class EveryBody implements Hello {
+            public String hello() {
+                return ("Hello Everybody!");
+            }      
+            void main(String... args) {
+              System.out.println(new Everybody().hello());
             }
         }
-                            `}
+
+        $javac EveryBody.java
+        $java  EveryBody.class
+        `}
                         </code>
                     </pre>
+
+                    <Zoom>
+                        <h1 className="welcome-important">'Hello Everybody!'</h1>
+                    </Zoom>
                     <br></br>
 
                     <blockquote>
                         I am called <span>Ricardo Ferreira</span> and as a <Link to="https://www.ferreiras.dev.br/cv/ResumeRicardoFerreira.pdf" target="_blank" className="contact-details"> Sr Software Engineer</Link>,
                         🛠️ loves to design, writes clean code and deploy web-based business applications.
-                        I have been very succesful in being the architect of solutions and applications that have met and even exceeded businesses
-                        and customers requirements in all companies I have been working for. For a comprehensive description of my journey,
-                        take a look at my history, clicking here. <Link to="/skills"><img src={history}></img></Link>
+                        I have been very succesful being the architect of solutions and applications that have met and even exceeded businesses
+                        and customers requirements in all companies I have been working for, specially in the digital payment industry, having deployes
+                        over the years, applications that helped retail stores increase their profitablity and keeping up with a
+                        fast changing payment requirements. For a comprehensive description of my journey and roadmap up to today,
+                        take a look at my history, clicking on the links below to find out more about my career. 
                     </blockquote>
+                    <br></br>
                 </Zoom>
 
                 <Zoom>
                     <div className="contact-me">
+                        <Link to="/history"><img src={history}></img></Link>
                         <Link to="/projects"><img src={codeLogo}></img></Link>
                         <Link to="/skills"><img src={skillLogo}></img></Link>
                         <Link to="/contacts"><img src={contactLogo}></img></Link>
