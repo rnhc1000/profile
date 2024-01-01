2039e7a2a1735d211b8cbba4a980720528977aa1
Ricardo Ferreira <rnhc1000@hotmail.com>
2024-01-01 15:58:53 -0300

    Improve UX thru more related icons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,9 @@
 import './styles.css';
 import homeLogo from '../../assets/house-check.svg';
-import skillLogo from '../../assets/tools.svg';
+import skillLogo from '../../assets/mortarboard.svg';
 import contactLogo from '../../assets/whatsapp.svg';
-import codeLogo from '../../assets/activity.svg';
-import history from '../../assets/history.svg';
+import codeLogo from '../../assets/tools.svg';
+import history from '../../assets/txt.svg';
 
 import { Link } from 'react-router-dom';
 
@@ -13,11 +13,12 @@ export default function Header() {
 
         <header className="nav-container">
             <nav id="navbar" className="nav-main">
-                <Link to='/'><img src={homeLogo}/></Link>
-                <Link to='/projects'><img src={codeLogo}/></Link>
-                <Link to='/skills'><img src={skillLogo}/></Link>
-                <Link to='/contacts'><img src={contactLogo}/></Link>
-                <Link to='history'><img src={history}/></Link>
+                <Link to='/'><img data-toggle="tooltip" data-placement="top" data-animation="" title="Home" src={homeLogo}/></Link>
+                <Link to='/history'><img data-toggle="tooltip" data-placement="top" data-animation="" title="History" src={history}/></Link>
+                <Link to='/skills'><img data-toggle="tooltip" data-placement="top" data-animation="" title="Skills" src={skillLogo}/></Link>
+                <Link to='/projects'><img data-toggle="tooltip" data-placement="top" data-animation="" title="Projects"src={codeLogo}/></Link>
+                <Link to='/contacts'><img data-toggle="tooltip" data-placement="top" data-animation="" title="Contact" src={contactLogo}/></Link>
+          
             </nav>
         </header>
     );
