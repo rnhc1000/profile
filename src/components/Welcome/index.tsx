639a59ab172616046d788d917ea802fc8cc30a3c
Ricardo Ferreira <rnhc1000@hotmail.com>
2024-02-13 17:54:20 -0300

    welcome component fixed to meet new UX suggestion to allow easier mobile navigation

--- src/components/Welcome/index.tsx
+++ src/components/Welcome/index.tsx
@@ -2,7 +2,6 @@ import './styles.css';
 
 import { Link } from 'react-router-dom';
 import Zoom from 'react-reveal/Zoom';
-import Rotate from 'react-reveal/Rotate';
 import skillLogo from '../../assets/mortarboard.svg';
 import contactLogo from '../../assets/whatsapp.svg';
 import codeLogo from '../../assets/tools.svg';
@@ -11,74 +10,72 @@ import Prism from 'prismjs';
 import 'prismjs/components/prism-clike';
 import "prismjs/themes/prism-tomorrow.css";
 import 'prismjs/plugins/line-numbers/prism-line-numbers.js'
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { ReactTyped } from "react-typed";
+
 
 export default function Welcome() {
-    useEffect(() => {
-        Prism.highlightAll()
-    }, [])
+  useEffect(() => {
+    Prism.highlightAll()
+  }, [])
 
-    return (
-        <section id="welcome-section">
-            <div className="welcome-container">
-                <Zoom>
-                    <pre>
-                        <code className="language-javascript">
-                            {`
-import java.util.profile.ferreiras.ricardo;
-public class Greetings implements 
-                        Hello, EveryBody {
-    @Override
-    public String hello() {
+  return (
+    <section id="welcome-section">
+      <div className="welcome-container">
+
+        <pre>
+          <code className="language-javascript">
+            {`
+    import java.util.profile.ricardo;
+    public class Greetings implements 
+                    Hello, EveryBody {
+      @Override
+      public String hello() {
         return "Hello, ";
-    }
-    @Override
-    public String everybody() {
+      }
+      @Override
+      public String everybody() {
         return "Everybody!";
-    }
+      }
 
-    public static void main(String... args) {
+      public static void main(String... args) {
         Greetings greetings = new Greetings();
-        System.out.println(
-            greetings.hello() + greetings.everybody());
-        }
-    }
-
-    $javac Greetings.java
-    $java  Greetings
-        
+          System.out.println(
+            greetings.hello() + 
+              greetings.everybody()
+          );
+      }
+    }   
 `}
-                        </code>
-                    </pre>
+          </code>
+        </pre>
 
-                    <Rotate>
-                        <h1 className="welcome-important">Hello, Everybody!</h1>
-                        
-                    </Rotate>
-                    <br></br>
+        <Zoom>
+          <ReactTyped style={{color: 'white'}}className="welcome-important" startWhenVisible strings={["Hello, Everybody!"]} typeSpeed={100} backSpeed={50}/>
+          <br></br>
 
-                    <blockquote>
-                        I am called <span>Ricardo Ferreira</span> and as a <Link to="https://www.ferreiras.dev.br/cv/ResumeRicardoFerreira.pdf" target="_blank" className="contact-details"> Sr Software Engineer</Link>,
-                        🛠️ loves to design, writes clean code and deploy web-based business applications.
-                        I have been very succesful being the architect and head developer of solutions and applications that 
-                        have met and even exceeded businesses and customers requirements in all companies I have been working for, 
-                        specially in the digital payments industry, having deployed over the years, applications that have helped retail 
-                        stores increase their profitablity and keeping up with a fast changing payment requirements. 
-                        For a comprehensive description of my journey and roadmap up to today,
-                        take a look at my history, clicking on the links below to find out more about my career.
-                    </blockquote>
-                    <br></br>
-                </Zoom>
+          <blockquote className="welcome-quote">
+            I am called <span>Ricardo Ferreira</span> and as a <Link to="https://www.ferreiras.dev.br/cv/ResumeRicardoFerreira.pdf" target="_blank" className="contact-details"> Sr Software Engineer</Link>,
+            🛠️ loves to design, writes clean code and deploy web-based business applications.
+            I have been very succesful being the architect and head developer of solutions and applications that
+            have met and even exceeded businesses and customers requirements in all companies I have been working for,
+            specially in the digital payments industry, having deployed over the years, applications that have helped retail
+            stores increase their profitablity and keeping up with a fast changing payment requirements.
+            For a comprehensive description of my journey and roadmap up to today,
+            take a look at my history, clicking on the links below to find out more about my career.
+          </blockquote>
+          <br></br>
+        </Zoom>
 
-                <Zoom>
-                    <div className="contact-me">
-                        <Link to='/history'><img data-toggle="tooltip" data-placement="top" data-animation="" title="History" src={history} alt="History"/></Link>
-                        <Link to='/skills'><img data-toggle="tooltip" data-placement="top" data-animation="" title="Skills" src={skillLogo} alt="Skills"/></Link>
-                        <Link to='/projects'><img data-toggle="tooltip" data-placement="top" data-animation="" title="Projects" src={codeLogo} alt="Projects"/></Link>
-                        <Link to='/contacts'><img data-toggle="tooltip" data-placement="top" data-animation="" title="Contact" src={contactLogo} alt="Contact"/></Link>
-                    </div>
-                </Zoom>
-            </div>
-        </section>
-    );
+        <Zoom>
+          <div className="contact-me">
+            <Link to='/history'><img data-toggle="tooltip" data-placement="top" data-animation="" title="History" src={history} alt="History" /></Link>
+            <Link to='/skills'><img data-toggle="tooltip" data-placement="top" data-animation="" title="Skills" src={skillLogo} alt="Skills" /></Link>
+            <Link to='/projects'><img data-toggle="tooltip" data-placement="top" data-animation="" title="Projects" src={codeLogo} alt="Projects" /></Link>
+            <Link to='/contacts'><img data-toggle="tooltip" data-placement="top" data-animation="" title="Contact" src={contactLogo} alt="Contact" /></Link>
+          </div>
+        </Zoom>
+      </div>
+    </section>
+  );
 }
