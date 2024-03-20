aee9443cd5259a0e283401e74672cda0891ea57e
Ricardo Ferreira <rnhc1000@hotmail.com>
2024-03-20 14:09:58 -0300

    refactored code at welcome page to beeter fit mobile

--- src/components/Welcome/index.tsx
+++ src/components/Welcome/index.tsx
@@ -28,34 +28,23 @@ export default function Welcome() {
               {`
       package br.dev.ferreiras.threads;
 
-      public class ThreadGreetings {
-        public static void executeAll() {
-         Thread thrdOne = new Thread(new Runnable() {
+      public class GreetingsThread {
+        static class Greetings implements Runnable{
+          public void start() {
+            Thread t = new Thread(this,"threadName");
+            t.start();
+          }
           @Override
           public void run() {
             System.out.print("Hello, ");
+            try {
+              Thread.sleep(1000);
+              System.out.print("Everybody!");
+            } catch (InterruptedException e) {
+              throw new RuntimeException();
+            }
           }
-        });
-        Thread thrdTwo = new Thread(new Runnable() {
-          @Override
-          public void run() {
-            System.out.print("Everybody!");
-          }
-        });
-    
-        List<Runnable> threads = new ArrayList<>();
-        threads.add(thrdOne);
-        threads.add(thrdTwo);
-    
-        for (Runnable task : threads) {
-          ((Thread) task).start();
-        }
-      }
-
-      public static void main(String... args) {
-        executeAll();
-      }
-    }   
+        } 
 `}
             </code>
           </pre>
